ea3cac9e00fb229ec4c2cd876c76047ef52d273b
PavelAnokhin <pav-anokhin@yandex.ru>
2021-09-03 18:24:45 +0300

    Initial first commit

---
+++ src/main/java/ru/netology/PersonBuilder.java
@@ -0,0 +1,41 @@
+package ru.netology;
+
+public class PersonBuilder {
+    private String name;
+    private String surname;
+    private Integer age;
+    private String adress;
+
+    public PersonBuilder setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public PersonBuilder setSurname(String surname) {
+        this.surname = surname;
+        return this;
+    }
+
+    public PersonBuilder setAge (int age) {
+        this.age = age;
+        return this;
+    }
+
+    public PersonBuilder setAddress(String address) {
+        this.adress = address;
+        return this;
+    }
+
+    public Person build() {
+        if  (name == null || surname == null) {
+            throw new IllegalArgumentException("Bad request: check input data (name or surname)");
+        }   if (age == null) {
+            return new Person(name, surname, null, adress);
+        }   if ( age < 0 || age > 100 ) {
+            throw new IllegalArgumentException("Bad request: check input data (age)");
+        }   else {
+            return new Person(name, surname, age, adress);
+        }
+    }
+
+}
